f3c11c074b48b1d2b7259e04eff750a4ff65efea
LUIS <luis.enrique.enc@gmail.com>
2023-02-03 15:36:15 -0600

    [ADD] Ejercicio 4 terminado

--- AppAlgoritmos/Controller/Ejercicios/Ejercicio4 /Ejercicio4ViewController.swift
+++ AppAlgoritmos/Controller/Ejercicios/Ejercicio4 /Ejercicio4ViewController.swift
@@ -9,21 +9,56 @@ import UIKit
 
 class Ejercicio4ViewController: UIViewController {
 
+    //MARK: - OUTLETS
+    
+    @IBOutlet weak var lblEjercicio4: UILabel!
+    @IBOutlet weak var lblTitleE4: UILabel!
+    @IBOutlet weak var txtCatetoA: UITextField!
+    @IBOutlet weak var txtCatetoB: UITextField!
+    @IBOutlet weak var lblResultHipotenusa: UILabel!
+    @IBOutlet weak var btnCalcularHip: UIButton!
+    
+    
+    
+    
+    //MARK: - VARIABLES
+    
+    var catetoA: Double = 0
+    var catetoB: Double = 0
+    
+    
+    //MARK: - LIFE · CYCLE
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
+    
+    
+    //MARK: - FUNCTIONS
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func saveValue() {
+        self.catetoA = Double(txtCatetoA.text ?? "0") ?? 0
+        self.catetoB = Double(txtCatetoB.text ?? "0") ?? 0
     }
-    */
+    
+    func hypotenuse(_ a: Double, _ b: Double) {
+        var hip = (a * a + b * b).squareRoot()
+        self.lblResultHipotenusa.text = "El valor de la Hipotenusa es \(hip)"
+    }
+    
+    //MARK: - ACTIONS
+
+    @IBAction func btnCalculateHip(_ sender: Any) {
+        saveValue()
+        hypotenuse(catetoA, catetoB)
+    }
+    
+    
+    
+    
+    
+    //MARK: - NAVIGATION
+
+
+
 
 }
