20def9027b3f85bd245b8de4cad890ffaca26cc4
LUIS <luis.enrique.enc@gmail.com>
2023-02-08 16:33:15 -0600

    [ADD] EJERCICIO 9 COMPLETO

--- AppAlgoritmos/Controller/Ejercicios/Ejercicio9/Ejercicio9ViewController.swift
+++ AppAlgoritmos/Controller/Ejercicios/Ejercicio9/Ejercicio9ViewController.swift
@@ -9,21 +9,57 @@ import UIKit
 
 class Ejercicio9ViewController: UIViewController {
 
+    //MARK: - OUTLET
+    
+    @IBOutlet weak var lblE9: UILabel!
+    @IBOutlet weak var lblTitleE9: UILabel!
+    @IBOutlet weak var txtIngNum: UITextField!
+    @IBOutlet weak var btnValidarNumImpar: UIButton!
+    @IBOutlet weak var lblResultadoE9: UILabel!
+    
+    //MARK: - VARIABLES
+    
+    var ingNum: Int = 0
+    
+    //MARK: - CYCLE ' LIFE
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    //MARK: - FUNCTIONS
+    
+    
+    func saveValuesE9(){
+        self.ingNum = Int(txtIngNum.text ?? "0") ?? 0
     }
-    */
+    
+    
+    func determNumImpar(numIng:Int){
+        if numIng % 2 == 0{
+            self.lblResultadoE9.text = "El numero no es primo."
+        }else{
+            self.lblResultadoE9.text = "Es un numero primo."
+
+        }
+    }
+    
+    //MARK: - ACTIONS
+    
+    
+    @IBAction func btnDefinNumPrimo(_ sender: Any) {
+        saveValuesE9()
+        determNumImpar(numIng: ingNum)
+    }
+    
+    
+    
+    
+    //MARK: - NAVIGATIONS
+    
+    
+    
+
+
+ 
 
 }
