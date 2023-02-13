ed6d4bff1ca59e7d97ebe5dd399ae54fd937fe34
LUIS <luis.enrique.enc@gmail.com>
2023-02-13 11:07:06 -0600

    [ADD] ejercicio 11 completo

--- AppAlgoritmos/Controller/Ejercicios/Ejercicio11/Ejercicio11ViewController.swift
+++ AppAlgoritmos/Controller/Ejercicios/Ejercicio11/Ejercicio11ViewController.swift
@@ -8,22 +8,61 @@
 import UIKit
 
 class Ejercicio11ViewController: UIViewController {
+    
+    //MARK: - OUTLETS
+    
+    @IBOutlet weak var lblE11: UILabel!
+    @IBOutlet weak var lblTitleE11: UILabel!
+    @IBOutlet weak var txtIngreseDato: UITextField!
+    @IBOutlet weak var btnConvertir: UIButton!
+    @IBOutlet weak var lblResultadoE11: UILabel!
+    
+    
+    //MARK: - VARIABLES
+    
+    var dato: Int = 0
 
+    
+    //MARK: - LIFE · CYCLE
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
+    //MARK: - FUNCTIONS
+    
+    
+/*     func saveValues(){
+        let : number = txtIngreseDato.text
+        let doubleValue = NSString(string : number!).doubleValue
+        self.dato.text = Double(txtIngreseDato. ?? "0") ?? 0    Versal = (Vel * 1000) / 3600
+    } */
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func saveValues(){
+        self.dato = Int(txtIngreseDato.text ?? "0" ) ?? 0
     }
-    */
+    
+    func convertirValue(verSal : Int){
+        let velocidad = (verSal * 1000) / 3600
+        self.lblResultadoE11.text = "Se convierten a \(velocidad) m/s"
+    }
+    
+    //MARK: - ACTIONS
+    
+    
+    @IBAction func btnConvertir(_ sender: Any) {
+        saveValues()
+        convertirValue(verSal: dato)
+    }
+    
+    
+    
+    
+    //MARK: - NAVIGATIONS
+    
+    
+    
+    
+
+ 
 
 }
